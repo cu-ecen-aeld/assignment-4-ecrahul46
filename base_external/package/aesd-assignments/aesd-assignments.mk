719a3b34658803bcf721aca62f96f5572e6f9de8
ecrahul46 <ec_rahul46@yahoo.com>
2023-05-07 18:48:28 +0530

    first_commit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,12 +5,12 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents #COMMIT VERSION NUMBER
+AESD_ASSIGNMENTS_VERSION = 'cbb7d91a6a080b0004e9436eec1cc74577f9b191'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+# Your site should start with git@github.com: #GITHUB REPOSITORY LINK
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-ecrahul46.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
